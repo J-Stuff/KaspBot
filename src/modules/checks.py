e1c147d40603581a539bc2bd8f3bdbf765d44203
J-Stuff <jstuff@j-stuff.net>
2023-07-09 17:43:26 +1200

    New checks implemented

--- src/modules/checks.py
+++ src/modules/checks.py
@@ -10,6 +10,18 @@ def is_main_guild(interaction:discord.Interaction):
     return interaction.guild.id == bot.EnumGulds.MAIN #type:ignore
 
 
+@staticmethod
+def is_dev_guild(interaction:discord.Interaction):
+    return interaction.guild.id == bot.EnumGulds.DEV #type:ignore
+
+
+@staticmethod
+def is_dm(interaction:discord.Interaction):
+    return interaction.guild is None
+
+@staticmethod
+def is_main_guild_or_dev_guild(interaction:discord.Interaction):
+    return interaction.guild.id in [bot.EnumGulds.MAIN, bot.EnumGulds.DEV] #type:ignore
 
 
 
@@ -18,5 +30,28 @@ def is_main_guild(interaction:discord.Interaction):
 async def is_main_guild_error(interaction:discord.Interaction, error:app_commands.AppCommandError):
     if isinstance(error, commands.CheckFailure):
         await interaction.response.send_message('This command can only be used in the main guild', ephemeral=True)
+    else:
+        raise error
+    
+
+@is_dev_guild.error
+async def is_dev_guild_error(interaction:discord.Interaction, error:app_commands.AppCommandError):
+    if isinstance(error, commands.CheckFailure):
+        await interaction.response.send_message('This command can only be used in the dev guild', ephemeral=True)
+    else:
+        raise error
+    
+
+@is_dm.error
+async def is_dm_error(interaction:discord.Interaction, error:app_commands.AppCommandError):
+    if isinstance(error, commands.CheckFailure):
+        await interaction.response.send_message('This command can only be used in a DM', ephemeral=True)
+    else:
+        raise error
+    
+@is_main_guild_or_dev_guild.error
+async def is_main_guild_or_dev_guild_error(interaction:discord.Interaction, error:app_commands.AppCommandError):
+    if isinstance(error, commands.CheckFailure):
+        await interaction.response.send_message('This command can only be used in the main guild or the dev guild', ephemeral=True)
     else:
         raise error
